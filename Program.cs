09619d3be763afa929579d04fd019f50b1f083c8
Aschu-3000 <schu0239@algonquinlive.com>
2023-03-15 11:35:53 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using PickRandomCards;
+using System.Transactions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter the number of cards to pick: ");
+        string line = Console.ReadLine();
+        if (int.TryParse(line, out int numberOfCards))
+        {
+            foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+            {
+                Console.WriteLine(card);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Please pick a valid number");
+        }
+    }
+}
